0a999cf19cde55427fc1701f899eb1df95a2f87b
Kadu Lemos <kadulemos@hotmail.com>
2020-07-17 10:47:59 -0300

    Boxplot, Boxe, Violin

--- 3_data_visualization_seaborn.py
+++ 3_data_visualization_seaborn.py
@@ -116,7 +116,106 @@ line_kws={'lw': 4})
 get_ipython().run_line_magic('matplotlib', 'inline')
 sns.lmplot(x='total_bill', y='tip', hue='smoker', col='time', row='sex', palette='Set2', data=tips)
 
+# %% [markdown]
+# <h2>Visualizando distribuições de observações</h2>
+# 
+# * Um dataset muito grande será difícil de visualizar variáveis categóricas;
+# * É preciso sumarizar a distribuição dos dados para facilitar a visualização;
+# * Podemos usar um gráfico do tipo Boxplot para visualizar a distribuição de tais variáveis;
+# * Esse tipo de gráfico é muito útil para visualizar ** *Outliers* **.
+# %% [markdown]
+# **Boxplot de Dias da semana por total de conta**
 
 # %%
+sns.catplot(x='day', y='total_bill', kind='box', data=tips)
+
+# %% [markdown]
+# **Gráfico de Boxplot dos dias por total de conta e se a pessoa é fumante ou não**
+
+# %%
+sns.catplot(x='day', y='total_bill', hue='smoker', kind='box', data=tips)
+
+# %% [markdown]
+# **Boxen**
+# 
+# * Tipo de boxplot com foco maior dos dados do que nos *outliers*;
+# * Esse tipo de gráfico é interessante quando temos grandes datasets.
+
+# %%
+sns.catplot(x='day', y='total_bill', kind='boxen', data=tips)
+
+# %% [markdown]
+# **Stripplot**
+# 
+# * Esse método permite plotar a distribuição dos dados;
+# * Podemos combinar os dois gráficos para ter mais informação.
+
+# %%
+sns.catplot(x='day', y='total_bill', kind='boxen', data=tips)
+sns.stripplot(x='day', y='total_bill', data=tips, color='blue')
+
+# %% [markdown]
+# ** O método *catplot()* permite usarmos facetgrid assim podemos combinar gráficos em uma única figura**
+
+# %%
+get_ipython().run_line_magic('matplotlib', 'inline')
+sns.catplot(x='sex', y='total_bill', hue='smoker', col='time', data=tips, kind='boxen', height=4)
+
+# %% [markdown]
+# <h2>Violin Plot</h2>
+# 
+# * Gráfico ideal para visualizar a distribuição de variáveis;
+# * Combinação do Boxplot com o KDE;
+# * Funciona bem para distribuições com picos;
+# * Permite uma visualização mais rica do que com o Boxplot normal. 
+# %% [markdown]
+# **Violin Plot**
+
+# %%
+sns.catplot(x='day', y='total_bill', kind='violin', data=tips)
+
+# %% [markdown]
+# **Violin Horizontal**
+
+# %%
+sns.catplot(x='total_bill', y='day', kind='violin', data=tips)
+
+# %% [markdown]
+# **Usando o parâmetro Hue para ter uma terceira variável**
+
+# %%
+sns.catplot(x='day', y='total_bill', hue='time', kind='violin', data=tips)
+
+# %% [markdown]
+# **Split**
+# 
+# * É possível dividir o violin plot com o parâmetro split;
+# * Sendo útil quando temos muitos dados para exibir.
+
+# %%
+sns.catplot(x='day', y='total_bill', hue='sex', split=True, kind='violin', data=tips)
+
+# %% [markdown]
+# **Inner**
+# 
+# * Usando o parâmetro *inner* para preencher o violin plot com observações;
+# * Útil para destacar bem as partes que possuem mais dados.
+
+# %%
+sns.catplot(x='day', y='total_bill', hue='sex', split=True, inner='stick', palette='pastel', kind='violin', data=tips)
+
+# %% [markdown]
+# **Swarm**
+# 
+# * Podemos combinar também um gráfico de swarm com o Violin Plot;
+# * Dessa forma fica mais explícito ainda a frequência de pontos nas partes do 'violino'.
+
+# %%
+sns.catplot(x='day', y='total_bill', hue='sex', split=True, palette='pastel', kind='violin', data=tips)
+sns.swarmplot(x='day', y='total_bill', color='k', size=3, data=tips)
+
+
+# %%
+
 
 
